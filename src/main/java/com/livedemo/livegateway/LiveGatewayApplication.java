ecbc0c93029399c55461801fcd61003822cdb46f
rocky <874904507@qq.com>
2021-09-16 23:09:05 +0800

    Initial commit

---
+++ src/main/java/com/livedemo/livegateway/LiveGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.livedemo.livegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/livedemo/livegateway/LiveGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.livedemo.livegateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
